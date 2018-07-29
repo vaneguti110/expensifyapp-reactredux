5cd80481ef1fda5142a372a1e6675de7835dfff4
Vanessa <vaneguti110@hotmail.com>
2018-07-29 23:08:06 +0200

    Create file expenses.js

---
+++ public/src/reducers/expenses.js
@@ -0,0 +1,30 @@
+//Expenses Reducer
+
+const expensesReducerDefaultState = [];
+export default (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({
+                id
+            }) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                };
+            });
+            //state.concat(action.expense) //concat combines values momentaneasly in an array, push it will add to array 
+        default:
+            return state;
+    }
+};
