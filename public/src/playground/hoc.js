b2d00179a1d472a004724f04ea4149e192026cbc
Vanessa <vaneguti110@hotmail.com>
2018-07-29 23:05:53 +0200

    Create hoc.js

---
+++ public/src/playground/hoc.js
@@ -0,0 +1,43 @@
+//Higher Order Component (HOC) - A component that render another component
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>The info is: {props.info}</p>
+    </div>
+);
+
+const withAdminWarming = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>This is private info. Please don't share!</p>}
+            <WrappedComponent {...props}/>
+        </div>
+    );
+};
+
+//requireAuthentication
+const requireAuthentication = (WrappedComponent) => {
+    return props => (
+        <div>
+            {props.isAuthenticated ? (
+                <WrappedComponent {...props} />
+
+            ) : (
+                <p>Please login to view the info</p>
+            )}
+        </div>
+    );
+};
+
+
+// const AdminInfo = withAdminWarming(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="There are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthentication = {true} info = "There are the details" />, document.getElementById('app'));
+
+
